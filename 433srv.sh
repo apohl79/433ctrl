7e2f457f012d798c2c82e53bd2bc50daa51e8d75
Andreas Pohl <apohl79@gmail.com>
2014-05-24 21:38:40 +0200

    refacturings

--- 433srv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR=/opt/433ctrl
-$DIR/433srv >/var/log/433srv.log 2>&1 &
-echo $! > /var/run/433srv.pid
-
--- comp.boot
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HOST=$1
-while [ -z "$(ping -c 1 $HOST|grep 'bytes from')" ]; do 
-    sleep 1
-done
